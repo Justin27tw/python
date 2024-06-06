0d688836f0ce5e29e4970589f879526f7c97bef8
Justin27tw <158735080+Justin27tw@users.noreply.github.com>
2024-06-06 20:20:38 +0800

    買鉛筆
    買鉛筆
    Problem D
    買鉛筆
    鉛筆一支5 元,一打50元。小明需要幫班上每位同學買一枝鉛筆,請問要多少錢?由於小明
    很注重環保,他絕不會為了省錢而多買任何不需要的東西。也就是說,小明買的鉛筆數量一
    定等於班上的人數。
    Input Format
    輸入只有一列,為小明班上的人數n,1 ≤ n ≤ 200。
    Output Format
    請輸出一個數字,代表這次採購的金額。
    Sample Input 1
    42
    Sample Input 2
    11
    Hint
    Sample Output 1
    180
    Sample Output 2
    55

---
+++ 0606.py
@@ -0,0 +1,15 @@
+n=int(input(""))
+price=5;
+price12=50;
+pen=12;
+total=0;
+index1=0;
+index2=0;
+if(n>=1 and n<=200):
+    total+=int(n/pen)*price12+int(n%pen)*price
+    print(total)
+    
+
+
+
+            
